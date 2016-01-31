30a819e3e9ae7b2e9f2f32aae809ac1e60d97b6b
haiyhaiy <haiy@haiyhaiys-MacBook-Air.local>
2016-01-31 11:34:58 +0800

    update

---
+++ README.txt
@@ -0,0 +1,28 @@
+uthor:haiyangfu512@gmail.com
+date: 2014年 5月 3日 星期六 14时25分24秒 CST
+
+说明：
+这个vim目录下面的配置文件主要是我自己
+的vim配置以及其他的如screen和tmux的配置。
+
+详情：
+
+vim配置
+basic.vim,amix的vim基本配置
+
+vim插件：
+    主要的vim插件有
+        插件管理， pathogen.
+        语法检查，syntastic
+        c+＋自动补全，clang
+        python自动补全，jedi
+        vim状态栏插件，airline
+        嵌入vim的中文输入，vimim
+
+tmux配置
+.tmux.conf
+
+screen配置
+.screenrc
+
+
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Begin to install pathogen..."
+echo "1 downloading..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "2 add to vimrc.."
+cat "execute pathogen#infect()
+syntax on
+filetype plugin indent on" >> $HOME/.vimrc
+
+echo "3 install syntastic"
+cd ~/.vim/bundle && \
+		git clone https://github.com/scrooloose/syntastic.git
+cat "set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0" >> $HOME/.vimrc
+
+echo "4 install jedi..."
+cd ~/.vim/bundle/ && git clone --recursive https://github.com/davidhalter/jedi-vim.git
+
+cat "autocmd FileType python setlocal completeopt-=preview
+" >> ~/.vimrc
+
+echo "5 install airline..."
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
